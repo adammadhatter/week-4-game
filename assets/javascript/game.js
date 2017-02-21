72895d4929705632673d49924dd84bd774f9ff84
Adam Brooks <adamgb5150@gmail.com>
2017-02-20 20:57:45 -0500

    week4hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,15 @@
 
+var chosenNumber = "";
+var numberList = []
+var numbersNeeded = 1
+var winCounter = 0;
+var lossCounter = 1;
 
-var = gameCount
-var = playerCount
+chosenNumber = numberList[Math.floor(Math.random() * numberList.length)];
+
+
+for( var i = 0; i < numbersNeeded; i++) {
+	var num = Math.floor(Math.random() * 120 + 1);
+	numberList.push(num);
+}
+console.log(numberList)
