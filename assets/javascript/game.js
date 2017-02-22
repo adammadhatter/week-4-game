08e0058f99c95b43a5081f138029af693d47d197
Adam Brooks <adamgb5150@gmail.com>
2017-02-22 17:02:36 -0500

    week4game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
 
-var chosenNumber = "";
-var numberList = []
+var chosenNumber = 0;
+var numberList = [];
 var numbersNeeded = 1
 var winCounter = 0;
 var lossCounter = 1;
-
-chosenNumber = numberList[Math.floor(Math.random() * numberList.length)];
 
 
 for( var i = 0; i < numbersNeeded; i++) {
 	var num = Math.floor(Math.random() * 120 + 1);
 	numberList.push(num);
 }
+chosenNumber = numberList[Math.floor(Math.random() * numberList.length)];
+document.getElementById("chosen-number").innerHTML = chosenNumber;
+
 console.log(numberList)
