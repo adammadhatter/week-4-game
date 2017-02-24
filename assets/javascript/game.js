a6f23ce9525fe54d033b9b8a74a69db13dcb406c
Adam Brooks <adamgb5150@gmail.com>
2017-02-24 08:40:41 -0500

    week4game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $( document ).ready(function(){
-  var Random=Math.floor(Math.random()*120+19) 
+  var Random=Math.floor(Math.random()*101+19) 
    // Selects a random number to be shown at the start of the game 
    // Number should be should be between 19 - 120 
    // 
